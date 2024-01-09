f3ac32c15e19bbd232b6e64110aece640f500440
NhatPhuong2106 <116291724+NhatPhuong2106@users.noreply.github.com>
2024-01-09 16:24:37 +0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -148,15 +148,13 @@ int main(int argc, char *argv[]) {
     std::cout << "Test acc: " << acc << std::endl;
     std::cout << "Time: " << ts << " ms" << std::endl;
     //
-    std::cout << std::endl << "--------------- Test an image------------------------" << std::endl;
+    std::cout << std::endl << "--------------- Test forward detail------------------------" << std::endl;
     int rand_num = rand() % dataset.test_data.cols();
-    Matrix x = dataset.test_data.col(rand_num);
-    dnn.forward(x);
     Matrix::Index max_index;
-    float max_value = dnn.output().col(0).maxCoeff(&max_index);
+    float max_value = dnn.output().col(rand_num).maxCoeff(&max_index);
     std::cout << "Get image number: " << rand_num << std::endl;
     std::cout << "Image has label: " << dataset.test_labels.col(rand_num) << std::endl;
-    std::cout << "Model has prediction:\n " << dnn.output().col(0) << std::endl;
+    std::cout << "Model has prediction:\n " << dnn.output().col(rand_num) << std::endl;
     std::cout << "Model predict label:\n " << int(max_index) << std::endl;
   }
 
