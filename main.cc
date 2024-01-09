aafd944e0c09ee2bc98810b623df32f192a1c5fa
NhatPhuong2106 <116291724+NhatPhuong2106@users.noreply.github.com>
2024-01-09 11:04:46 +0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -152,12 +152,10 @@ int main(int argc, char *argv[]) {
     int rand_num = rand() % dataset.test_data.cols();
     Matrix x = dataset.test_data.col(rand_num);
     dnn.forward(x);
-    Matrix res = dnn.output().col(0);
     Matrix::Index max_index;
-    float max_value = res.maxCoeff(&max_index);
+    float max_value = dnn.output().col(0).maxCoeff(&max_index);
     std::cout << "Image has label: " << dataset.test_labels.col(rand_num) << std::endl;
-    std::cout << "Image has prediction:\n " << res << std::endl;
-    std::cout << "Image has predict label:\n " << max_value << std::endl;
+    std::cout << "Image has prediction:\n " << dnn.output().col(0) << std::endl;
     std::cout << "Image has predict label:\n " << int(max_index) << std::endl;
   }
 
